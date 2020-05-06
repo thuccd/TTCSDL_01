62c5944d176428912b14f3d635e9935117030b83
unknown <ducthucchu@gmail.com>
2020-05-07 00:10:09 +0700

    ThucCD

---
+++ CAFE_VIP/Controllers/QL_SanPhamController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CAFE_VIP.Models;
+using Models.EF;
+using CAFE_VIP.Common;
+
+namespace CAFE_VIP.Controllers
+{
+    public class QL_SanPhamController : Controller
+    {
+        // GET: QL_SanPham
+        CafeDbContext db = new CafeDbContext();
+        public ActionResult Index()
+        {
+            return View(db.PRODUCTs.Where(n=>n.ProductStatus==false));
+        }
+    }
+}
